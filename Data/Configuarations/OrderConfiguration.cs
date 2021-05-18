b0eaabd3e4c489d9043b6957fce43feffcbfbbff
DESKTOP-F3CJDHS\Phuc <phamphuc11296@gmail.com>
2021-05-18 17:59:16 +0700

    Configuaration + Migiration DB

---
+++ Data/Configuarations/OrderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Configuarations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Order");
+
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
