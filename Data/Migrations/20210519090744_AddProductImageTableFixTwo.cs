e9a1f1333314c98e89eda81b1c716f04eaf280d9
DESKTOP-F3CJDHS\Phuc <phamphuc11296@gmail.com>
2021-05-19 16:08:13 +0700

    Migrations

---
+++ Data/Migrations/20210519090744_AddProductImageTableFixTwo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddProductImageTableFixTwo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isDefault",
+                table: "ProductImages",
+                newName: "IsDefault");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "FileSize",
+                table: "ProductImages",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "AppRole",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "30278c6d-95fe-4576-9e37-84e3a70aace2");
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "7a528c83-e781-4d9b-bfe7-f3c74890bab7", "AQAAAAEAACcQAAAAENandn5m+PzsgCTdQv/99brr31dB0GkYrsWnMam98DZOYFYXrK54+m4xgV2/LSeEpA==" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 5, 19, 16, 7, 44, 99, DateTimeKind.Local).AddTicks(5868));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDefault",
+                table: "ProductImages",
+                newName: "isDefault");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FileSize",
+                table: "ProductImages",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.UpdateData(
+                table: "AppRole",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
+                column: "ConcurrencyStamp",
+                value: "fbcffc90-46b1-432e-bc8a-86b5e26fca0a");
+
+            migrationBuilder.UpdateData(
+                table: "AppUser",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "df5fd210-9e24-43a4-88a6-d0494194d779", "AQAAAAEAACcQAAAAEHi7PtbvTxlC0fbgF12UXgdyKpWiGcQUQwRUY9O9C30QpY1SclU8v7p3oIUvQTFanw==" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 5, 19, 15, 13, 35, 626, DateTimeKind.Local).AddTicks(2651));
+        }
+    }
+}
